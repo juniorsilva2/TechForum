66b5c8f84db6eff95f559095bda2f23c08898432
Junior Silva <junior_silva158@hotmail.com>
2023-02-04 00:31:00 -0300

    Refactoring logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -48,7 +48,6 @@ const login = async (req, res, next) => {
 
 const logout = async (req, res) => {
   try {
-    req.session = null;
     req.logout(function (err) {
       if (err) return next(err);
       res.redirect("/");
