43b02a01fedb27744ac0d0b8c4d09166a2677dbb
Junior Silva <junior_silva158@hotmail.com>
2023-11-20 21:07:50 -0300

    adding alterations for local user

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -15,6 +15,28 @@ const getTopics = async (req, res) => {
   }
 };
 
+const createTopic = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(404).json({ message: "Fields missing" });
+
+    const topicExist = await TopicModel.findOne({ name: name });
+    if (topicExist)
+      return res.status(422).json({ message: "This topic already exists" });
+
+    await TopicModel.create({ name });
+
+    const topic = await TopicModel.findOne({ name: name });
+    res.status(200).json({ message: "Topic sucessfully created", topic });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Server side error ocurred" });
+  }
+};
+
 module.exports = {
   getTopics,
+  createTopic,
 };
+
+
--- src/routes/private/privateTopicRoutes.js
+++ src/routes/private/privateTopicRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // const authenticated = require("../../middleware/authenticated");
 const topicController = require("../../controllers/topicController");
 
-// router.post("/api/topic", topicController.createTopic);
+ router.post("/api/topic", topicController.createTopic);    
 // router.get("/api/topic/:id", checkToken, topicController.getTopic);
 // router.get("/api/topics", checkToken, topicController.getTopics);
 // router.put("/api/topic/:id", checkToken, topicController.updateTopic);
